49f3a39c51038ea14ff1021a723e1b6d49821c46
oscarnar <orosas58@gmail.com>
2016-09-12 20:05:02 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+main()
+{
+    char mayu,minu;
+    scanf("%c",&mayu);
+    minu=mayu+32;
+    printf("%c",minu);
+
+    return 0;
+}
+int main()
+{
+    strindex(s,t)file:///home/cs22/Desktop/mayus/main.c
+
+}
+
+int main()
+{
+    char palabra[]={hola};
+    inver[strlen]=inver[strlen-1]
+}
